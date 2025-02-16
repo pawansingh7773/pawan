9e14b6b930d9af3d938b8c4994c18783dc8fc517
pawan <sodhapawan@gamail.com>
2025-02-16 16:36:04 +0530

    fixed everything

--- about.java
+++ about.java
@@ -1,5 +1,10 @@
+
+import java.util.Scanner;
 public class about{
     public static void main(String[] args) {
         System.out.println("OK HERE");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        sc.close();
     }
 }
